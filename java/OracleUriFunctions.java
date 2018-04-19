dcfe732d2327db1efbf85ecacb7386e36cd24887
Coder <coderdba@users.noreply.github.com>
2018-04-19 19:15:50 +0530

    Create OracleUriFunctions.java

---
+++ java/OracleUriFunctions.java
@@ -0,0 +1,37 @@
+package com.gm.rest.spring.oracle1;
+
+import java.lang.ProcessBuilder;
+import java.util.*;
+import java.io.File;
+import java.io.IOException;
+
+public class OracleUriFunctions {
+	
+	public void oracleAnsible(String schemaName, String schemaAction) throws Exception {
+
+		int exitCode;
+		String extraVarSchemaName   = "schema_name=" + schemaName;
+		String extraVarSchemaAction = "schema_action=" + schemaAction;
+		
+		ProcessBuilder pb = new ProcessBuilder("/usr/local/bin/ansible-playbook", 
+				"-s", "/Users/username/twork/oracle_saas/ansible/oracle_saas1/oracle_saas.yml", 
+				"-i", 
+				"/Users/username/opt/ansible/hosts", 
+				"-u", "ansible",
+				"-e",
+				extraVarSchemaName, "-e", extraVarSchemaAction);
+
+		System.out.println (pb.command().toString());
+		
+		File logfile = new File("/tmp/javaansible.log");
+
+		pb.redirectErrorStream(true);
+		pb.redirectOutput(logfile);
+		pb.directory(new File("/Users/username//twork/oracle_saas/ansible/oracle_saas1"));
+		Process p = pb.start();
+		
+		exitCode = p.waitFor();
+
+	}
+
+}
