dd64ab17c8b62ed86fbaba832807563608f6e111
Coder <coderdba@users.noreply.github.com>
2018-04-19 19:13:12 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,12 @@
+package com.gm.rest.spring.oracle1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
