840b5622f6e49e9253eeb1ebb32bfbd4e88030e4
Coder <coderdba@users.noreply.github.com>
2018-04-24 12:17:00 +0530

    Update OracleUriFunctions.java

--- java/OracleUriFunctions.java
+++ java/OracleUriFunctions.java
@@ -7,7 +7,37 @@ import java.io.IOException;
 
 public class OracleUriFunctions {
 	
-	public void oracleAnsible(String schemaName, String schemaAction) throws Exception {
+	public int oracleAnsible(String schemaName, String schemaAction) throws Exception {
+
+		int exitCode;
+		String extraVarSchemaName   = "schema_name=" + schemaName;
+		String extraVarSchemaAction = "schema_action=" + schemaAction;
+		
+		ProcessBuilder pb = new ProcessBuilder("/usr/local/bin/ansible-playbook", 
+				"-s", "/Users/username/twork/oracle_saas/ansible/oracle_saas1/oracle_saas.yml", 
+				"-i", 
+				"/Users/username/opt/ansible/hosts", 
+				"-u", "ansible",
+				"-e",
+				extraVarSchemaName, "-e", extraVarSchemaAction);
+
+		System.out.println (pb.command().toString());
+		
+		File logfile = new File("/tmp/javaansible.log");
+
+		pb.redirectErrorStream(true);
+		pb.redirectOutput(logfile);
+		pb.directory(new File("/Users/username//twork/oracle_saas/ansible/oracle_saas1"));
+		Process p = pb.start();
+		
+		exitCode = p.waitFor();
+		
+		return exitCode;
+
+	}
+	
+	// does not return returncode of ansible operation
+	public void oracleAnsibleVoid(String schemaName, String schemaAction) throws Exception {
 
 		int exitCode;
 		String extraVarSchemaName   = "schema_name=" + schemaName;
@@ -33,5 +63,4 @@ public class OracleUriFunctions {
 		exitCode = p.waitFor();
 
 	}
-
 }
